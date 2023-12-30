f67f88f5120745d8ccb734ff41fdd0f7a6bbf4cc
dnldl <dnldlscjf@gmail.com>
2023-12-30 14:31:50 +0900

    setting format

---
+++ src/main/java/org/example/CRUD.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class CRUD {
+    public static int addData() {
+        System.out.println("<1> 동영상 추가");
+        System.out.println("=======================");
+        System.out.println("추가하고 싶은 영상의 이름을 입력하세요");
+
+        System.out.println("영상의 크리에이터를 입력하세요");
+
+        System.out.println("영상의 좋아요 갯수를 입력하세요");
+
+        System.out.println("데이터가 추가되었습니다");
+        return 1;
+    }
+    public static void searchData() {
+    }
+
+    public static void showData() {
+    }
+
+    public static void deleteData() {
+    }
+
+    public static void editData() {
+    }
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import org.example.data;
+import static org.example.CRUD.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int input = 0;
+        int count =0;
+        boolean repeat = true;
+        ArrayList list = new ArrayList();
+        while(repeat){
+            showMenu();
+            input = s.nextInt();
+            switch (input){
+                case 1:
+                    addData();
+                    break;
+                case 2:
+                    editData();
+                    break;
+                case 3:
+                    deleteData();
+                    break;
+                case 4 :
+                    showData();
+                    break;
+                case 5 :
+                    searchData();
+                    break;
+                case 0:
+                    repeat=false;
+                    break;
+                default:
+                    System.out.println("잘못된 입력 입니다");
+                    break;
+            }
+
+        }
+        System.out.println("프로그램이 종료되었습니다!");
+    }
+
+
+
+    public static void showMenu() {
+        System.out.println("=============[ 보고 싶은 너튜브 ] ==========");
+        System.out.println("1. 영상 추가");
+        System.out.println("2. 영상 수정");
+        System.out.println("3. 영상 삭제");
+        System.out.println("4. 영상 전체 보기");
+        System.out.println("5. 영상 검색");
+        System.out.println("0. 프로그램 종료");
+        System.out.println("====================================");
+        }
+}
