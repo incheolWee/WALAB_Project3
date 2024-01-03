36a62c719f1675f093a2f4e472e7032fe7ff5219
dnldl <dnldlscjf@gmail.com>
2024-01-03 16:51:31 +0900

    필요없는 import 삭제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
+
 
 
 
